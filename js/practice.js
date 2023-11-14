a36a67bd19190344d112f035d1b9a82884135e54
DmFet <148346250+DmFet@users.noreply.github.com>
2023-11-14 18:29:05 +0300

    added practice.js

---
+++ js/practice.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const options = {
+  name: "test",
+  width: 1024,
+  height: 1024,
+  colors: {
+    border: "black",
+    background: "red",
+  },
+};
+console.log(options.name);
